cecb982d997ca3009e33a96e5d44bbdcedff8550
shulamit <shlamit931@gmail.com>
2023-04-18 14:36:16 +0300

    fix origin

--- Program.cs
+++ Program.cs
@@ -63,7 +63,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("OpenPolicy",
                           policy =>
                           {
-                              policy.WithOrigins("http://localhost:3000")
+                              policy.WithOrigins("http://localhost:3000","https://todosreactclient.onrender.com")
                                                   .AllowAnyHeader()
                                                   .AllowAnyMethod();
                           });
